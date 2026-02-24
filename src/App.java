c687b1c430618e00c3a7c5b7c34c45f5f23be712
umer <syedumerahmedcode@gmail.com>
2026-02-24 16:39:41 +0500

    Feature: Implemented the solution and tested it against a valid test case scenario.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * Given an m x n matrix, return all elements of the matrix in spiral order.
+        
+        
+        
+        Example 1:
+        
+        
+        Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+        Output: [1,2,3,6,9,8,7,4,5]
+        Example 2:
+        
+        
+        Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+        Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+         */
+        int[][] matrix={
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12}
+        };
+        Solution solution = new Solution();
+        List<Integer> result = solution.spiralOrder(matrix);
+        System.out.println("The result is: "+result);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
-     public List<Integer> spiralOrder(int[][] matrix) {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int visited = 10001;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int row = 0, column = 0;
+        //directions:::right, down, left, up
+        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+        int currentDirection = 0, changedDirection = 0;
+        List<Integer> ans = new ArrayList<>();
+        ans.add(matrix[0][0]);
+        matrix[0][0] = visited;
+        //Why 2? right+down OR left+up
+        while (changedDirection < 2) {
+             while(row + directions[currentDirection][0] >= 0 &&
+                    row + directions[currentDirection][0] < rows &&
+                    column + directions[currentDirection][1] >= 0 && 
+                    column + directions[currentDirection][1] < columns &&
+                    matrix[row+directions[currentDirection][0]][column + directions[currentDirection][1]] != visited){
+
+                changedDirection = 0;
+                row = row + directions[currentDirection][0];
+                column = column + directions[currentDirection][1];
+
+                ans.add(matrix[row][column]);
+                matrix[row][column] = visited;
+            }
+            currentDirection = (currentDirection + 1) % 4;
+            changedDirection++;
+        }
+        return ans;
         
     }
 
